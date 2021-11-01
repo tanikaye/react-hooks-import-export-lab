63314640bd1687d7e3e761b4da19f43221d01bf0
Tani Kaye <1tanikaye@gmail.com>
2021-11-01 11:57:23 -0400

    commit tree cbbe24547f54a1d0fd0dbf2c492ecf9c46942429

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,10 @@
 import React from "react";
+import About from './About.js'
+import Home from './Home.js'
+import NavBar from './NavBar.js'
+import {username, city, image} from '../data/user.js'
+
+
 
 function App() {
   return (
